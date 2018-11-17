6a28bb5118a6a4abc3efa130bc2898832845972f
James Mwangi <karokijames40@gmail.com>
2018-11-17 09:27:49 +0300

    Update landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -163,45 +163,45 @@
         </div>
 
         <div class="card-group" style="margin: 1%;">
-            <div class="card">
+            <div class="card" style="border: none; background-color: black;">
                 <img class="card-img-top" src="{{ asset('img/ball.jpg') }}" alt="Card image cap">
-                <div class="card-body">
+                {{--<div class="card-body">--}}
                     {{--<h5 class="card-title">Soccer</h5>--}}
                     {{--<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>--}}
                     {{--<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>--}}
-                </div>
+                {{--</div>--}}
             </div>
-            <div class="card">
+            <div class="card" style="border: none; background-color: black;">
                 <img class="card-img-top" src="{{ asset('img/rally.jpg') }}" alt="Card image cap">
-                <div class="card-body">
+                {{--<div class="card-body">--}}
                     {{--<h5 class="card-title">Rally</h5>--}}
                     {{--<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>--}}
                     {{--<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>--}}
-                </div>
+                {{--</div>--}}
             </div>
-            <div class="card">
+            <div class="card" style="border: none; background-color: black;">
                 <img class="card-img-top" src="{{ asset('img/bb.jpg') }}" alt="Card image cap">
-                <div class="card-body">
+                {{--<div class="card-body">--}}
                     {{--<h5 class="card-title">Basket Ball</h5>--}}
                     {{--<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>--}}
                     {{--<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>--}}
-                </div>
+                {{--</div>--}}
             </div>
-            <div class="card">
+            <div class="card" style="border: none; background-color: black;">
                 <img class="card-img-top" src="{{ asset('img/pool.jpg') }}" alt="Card image cap">
-                <div class="card-body">
+                {{--<div class="card-body">--}}
                     {{--<h5 class="card-title">Swimming</h5>--}}
                     {{--<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>--}}
                     {{--<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>--}}
-                </div>
+                {{--</div>--}}
             </div>
-            <div class="card">
+            <div class="card" style="border: none; background-color: black;">
                 <img class="card-img-top" src="{{ asset('img/athlete-bike.jpg') }}" alt="Card image cap">
-                <div class="card-body">
+                {{--<div class="card-body">--}}
                     {{--<h5 class="card-title">Cycling</h5>--}}
                     {{--<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>--}}
                     {{--<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>--}}
-                </div>
+                {{--</div>--}}
             </div>
         </div>
 
